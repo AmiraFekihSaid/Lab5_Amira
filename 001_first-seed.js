aeeb5487c4e23bc388b61c385614c132b6a5b080
AmiraFekihSaid <64223246+AmiraFekihSaid@users.noreply.github.com>
2021-12-07 14:51:49 +0100

    Add files via upload

---
+++ 001_first-seed.js
@@ -0,0 +1,35 @@
+
+const profiles = [ 
+  { 
+    name: 'Imen' 
+  }, 
+  { 
+    name: 'Aymen' 
+  }, 
+  { 
+    name: 'Feres' 
+  }, 
+  { 
+    name: 'Nadia' 
+  }, 
+  { 
+    name: 'Mohamed' 
+  } 
+];
+
+exports.seed = function(knex) {
+  return knex('profiles').del() 
+  .then(() => { 
+    return knex('profiles').insert(profiles) 
+  })
+  // // Deletes ALL existing entries
+  // return knex('table_name').del()
+  //   .then(function () {
+  //     // Inserts seed entries
+  //     return knex('table_name').insert([
+  //       {id: 1, colName: 'rowValue1'},
+  //       {id: 2, colName: 'rowValue2'},
+  //       {id: 3, colName: 'rowValue3'}
+  //     ]);
+  //   });
+};
